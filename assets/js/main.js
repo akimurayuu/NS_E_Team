04312127a399e2d005679ca42e8587159c9f9d20
AKIMURA_YUU <89886977+akimurayuu@users.noreply.github.com>
2021-09-21 17:30:14 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,20 +19,22 @@ const app = Vue.createApp({
       correctAnswer: {
         stage1: {
           q1: 'PeoPle',
+        　　　　q2: 'people',
+          q3: '@PeoPle_PeoPle_N'
         },
         stage2: {
           q1: 'マイナス',
-          // q2: 'えええ',
-          // q3: 'おおお'
+          q2: 'まいなす',
+          q3: '-'
         },
         stage3: {
           q1: 'タスケテ',
-          // q2: 'かかか',
+          q2: 'たすけて',
           // q3: 'ききき',
         },
          stage4: {
           q1: 'ピース',
-          // q2: 'かかか',
+          q2: 'ぴーす',
           // q3: 'ききき',
         }
       },
